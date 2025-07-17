4a235df05e5a7b30b85b1e3cbff2d00c0a77d6d7
Harsh Mishra <148937385+harshmishra00@users.noreply.github.com>
2025-07-17 14:20:14 +0530

    Create Leetcode-136.cpp

---
+++ Leetcode-136.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    int n,val,ans=0;
+    cout<<"Enter the length of array: ";
+    cin>>n;
+    vector<int> vec;
+    for(int i=0;i<n;i++){
+        cin>>val;
+        vec.push_back(val);
+    }
+    for(int i=0;i<n;i++){
+        ans=vec[i]^vec[i+1];
+        
+    }
+    
+    cout<<ans;;
+
+    return 0;
+}
