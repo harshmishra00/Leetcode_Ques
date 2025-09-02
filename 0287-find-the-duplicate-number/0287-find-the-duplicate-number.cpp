0d0a4a1065e8ca0706b44f8ee8c6798c7264d938
Harsh Mishra <148937385+harshmishra00@users.noreply.github.com>
2025-09-02 18:55:51 +0530

    Time: 70 ms (16.68%), Space: 88 MB (10.96%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& vec) {
+        unordered_set<int> s;
+        for(int val : vec){
+            if(s.find(val)!=s.end()){
+                return val;
+            }
+            s.insert(val);
+        }
+        return -1;
+    }
+};
