91218fc9b6b474af9a17bd27a6f86289991f75b0
Harsh Mishra <148937385+harshmishra00@users.noreply.github.com>
2025-10-08 18:59:36 +0530

    Time: 47 ms (39.82%), Space: 125.1 MB (65.71%) - LeetHub

---
+++ 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
+        sort(potions.begin(), potions.end());
+        int potionCount = potions.size();
+        vector<int> pairsCount(spells.size());
+
+        for (int i = 0; i < spells.size(); i++) {
+            long long spellPower = spells[i];
+            long long minPotionNeeded = (success + spellPower - 1) / spellPower;
+
+            int index = lower_bound(potions.begin(), potions.end(), minPotionNeeded) - potions.begin();
+            int successfulCount = potionCount - index;
+
+            pairsCount[i] = successfulCount;
+        }
+
+        return pairsCount;
+    }
+};
