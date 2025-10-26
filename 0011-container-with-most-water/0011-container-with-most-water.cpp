a2836d63dc87f679aa1a0cfc35327b2b30aff6fb
Harsh Mishra <148937385+harshmishra00@users.noreply.github.com>
2025-10-26 13:53:26 +0530

    Time: 0 ms (100%), Space: 63 MB (12.9%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -4,18 +4,17 @@ public:
         int n=arr.size();
         int left=0;
         int right=n-1;
-        int maxArea=INT_MIN;
+        int maxArea=0;
         while(left<right){
-            int width =right-left;
-            int height=min(arr[right],arr[left]);
-            int area=width*height;
-            maxArea=max(maxArea,area);
+            int height=min(arr[left],arr[right]);
+            int width=right-left;
+            long long area=(long long)height*width;
+            maxArea=max((int)area,maxArea);
             if(arr[left]<arr[right]){
                 left++;
             }else{
                 right--;
             }
-            
         }
         return maxArea;
     }
