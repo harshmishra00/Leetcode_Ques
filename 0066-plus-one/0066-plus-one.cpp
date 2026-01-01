28ff636ec9fa5ea49cdebb89b28a614692d27d6b
Harsh Mishra <148937385+harshmishra00@users.noreply.github.com>
2026-01-01 16:53:07 +0530

    Time: 0 ms (100%), Space: 11.6 MB (14.65%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -1,18 +1,30 @@
 class Solution {
 public:
+    bool isAll9(vector<int>& digits){
+        bool ans=1;
+        for(int d: digits)
+            ans=( ans & d==9);
+        return ans;
+    }
+
     vector<int> plusOne(vector<int>& digits) {
         int n=digits.size();
-        for(int i=n-1;i>=0;i--){
-            if(digits[i]<9){
-                digits[i]+=1;
+        if (isAll9(digits))
+        {
+            digits.assign(n+1, 0);
+            digits[0]=1;
+            return digits;
+        }
+        else
+        {
+            digits[n-1]++;
+            if (digits[n-1]!=10) return digits;
+            else {
+                digits.pop_back();
+                digits=plusOne(digits);
+                digits.push_back(0);
                 return digits;
             }
-            else{
-                digits[i]=0;
-            }
-            
         }
-        digits.insert(digits.begin(), 1); 
-        return digits;
     }
 };
