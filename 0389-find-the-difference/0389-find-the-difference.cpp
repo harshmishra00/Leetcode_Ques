4c139f9a716fe05e09125a565fa50ab43f9fabcb
Harsh Mishra <148937385+harshmishra00@users.noreply.github.com>
2026-08-07 00:38:24 +0530

    Time: 1 ms (19.73%), Space: 9.3 MB (26%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        int Xor=0;
-        for(char ch : s){
-            Xor^=ch;
-        }
+        unordered_map<char,int> mp;
+
+        for(char ch : s)
+            mp[ch]++;
+
         for(char ch : t){
-            Xor^=ch;
+            mp[ch]--;
+            if(mp[ch] < 0)
+                return ch;
         }
-        return (char)Xor;
+
+        return ' ';
     }
 };
